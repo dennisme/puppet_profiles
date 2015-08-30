9b259bf5de7a3ce2438228fd73d1eea3ff666838
Matthew Dennison <medennison123@gmail.com>
2015-08-29 20:55:16 -0400

    fixed require errors

--- manifests/base.pp
+++ manifests/base.pp
@@ -3,39 +3,39 @@ class profiles::base {
   
   package { 'htop': 
     ensure  => present,
-    require => Package['epel'],
+    require => Class['epel'],
   }
   
   package { 'itop': 
     ensure => present,
-    require => Package['epel'],
+    require => Class['epel'],
+  }
+  
+  package { 'atop':
+    ensure => present,
+    require => Class['epel'],
   }
   
   package { 'git':
     ensure => present,
-    require => Package['epel'],
-  }
-  
-  package { 'git':
-    ensure => present,
-    require => Package['epel'],
+    require => Class['epel'],
   }
   
   package { 'bind-utils':
     ensure => present,
-    require => Package['epel'],
+    require => Class['epel'],
   }
   
   package { 'nc':
     ensure => present,
-    require => Package['epel'],
+    require => Class['epel'],
   }
 
   user { 'dennisme':
      ensure   => 'present',
      home     => '/home/dennisme',
      password => '$6$6xtHwBV0$jR9ikiC79tC2pXQXsjMwwnNzeDWUST4TEUbkpMQvtARCh0SwTztHSL0UFjm8h8Ekkq7/RJyRRncjJVWq9N/wZ/',
-     shell    => '/bin/bash'
-     groups   => 'wheel'
+     shell    => '/bin/bash',
+     groups   => 'wheel',
   }
 }
